c9dadf60acd50d957b07e230170d8fa271889311
dravandi <55105397+dravandi@users.noreply.github.com>
2019-09-09 17:14:41 -0700

    Add files via upload

---
+++ Poem.py
@@ -0,0 +1,14 @@
+print("Roses are red")
+print("Violets are blue")
+print("Sugar is sweet")
+print("But I have commitment issues")
+print("So I'd rather just be friends")
+print("At this point in  our relationship")
+
+
+
+
+
+
+
+
---
+++ Temperature.py
@@ -0,0 +1,13 @@
+Celsius = 10
+Fahrenheit = ((Celsius * 9/5) + 35)
+print("Celsius is 10 and Fahrenheit is" + str(Fahrenheit))
+
+
+
+
+
+
+
+
+
+
